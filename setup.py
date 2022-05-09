a32b6ab99d2610da393b5d22988478fc3a8c721a
awesome-sse <sse103@mail.ru>
2022-05-09 20:01:18 +0300

    Init project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ml in prodaction homework1',
+    author='Stanislav',
+    license='',
+    entry_points={
+        "console_scripts": [
+            "train_model = src.train_pipeline:train_pipeline_command",
+            "predict_model = src.train_pipeline:predict_pipeline_command"
+        ]
+    },
+)
